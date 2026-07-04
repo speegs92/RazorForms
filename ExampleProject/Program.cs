c6bbc164ca4a1c39db2c327bbd517aa866c51de7
speegs92 <christian@levesque.dev>
2026-07-04 09:54:37 -0500

    changed example project to use Bootstap5

--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -9,13 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-builder.Services.UseRazorForms<CustomOptions>(CustomSetup);
+// builder.Services.UseRazorForms<CustomOptions>(CustomSetup);
 
 // builder.Services.UseRazorFormsWithMaterialize<MaterializeOptions>(MaterializeSetup);
 
 // builder.Services.UseRazorFormsWithBulma<RazorFormsOptions>(BulmaSetup);
 
-// builder.Services.UseRazorFormsWithBootstrap5<RazorFormsOptions>(Bootstrap5Setup);
+builder.Services.UseRazorFormsWithBootstrap5<RazorFormsOptions>(Bootstrap5Setup);
 // builder.Services.UseRazorFormsWithBootstrap5FloatingLabels<RazorFormsOptions>(Bootstrap5Setup);
 
 var app = builder.Build();
